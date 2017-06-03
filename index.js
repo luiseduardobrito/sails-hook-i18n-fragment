cdb9431895154d28a5496ddfda88eaa04624410b
Luis Eduardo Brito <luiseduardo14@gmail.com>
2017-06-03 15:21:54 -0300

    Feat: Cache implemented

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = function (sails) {
   var fs = require('fs');
   var marked = require('marked');
   var pkg = require('./package.json');
+  var cache = require('memory-cache');
 
   var configKey = pkg.name.replace('sails-hook-', '');
 
@@ -45,12 +46,31 @@ module.exports = function (sails) {
    * @param {function} callback
    */
   function getFragment (address, callback) {
+
     var path = getFragmentPath(address, activeLocale);
+    var cached = cache.get(path);
+
+    if(cached) {
+      callback(null, cached);
+    }
+
     fs.readFile(path, 'utf8', function (error, source) {
+
       if (error) {
         return callback(error);
       }
-      marked(source, callback);
+
+      marked(source, function (error, html) {
+
+        if (error) {
+          return callback(error);
+        }
+
+        cache.put(path, html);
+        callback(null, html);
+
+      });
+
     });
   }
 
